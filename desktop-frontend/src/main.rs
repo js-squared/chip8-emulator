1f0929238e6e3c887b9aec76ffc7fc8d6962fb93
Jesus Antonio Solis <solisjesus700@gmail.com>
2024-10-21 03:59:17 -0400

    formatting and added loading of rom file

--- desktop-frontend/src/main.rs
+++ desktop-frontend/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::Read;
 use backend::*;
 use sdl2::event::Event;
 
@@ -16,17 +18,27 @@ fn main() {
     // Setup SDL
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let window = video_subsystem
-    .window("Chip-8 Emulator", WINDOW_WIDTH, WINDOW_HEIGHT)
-    .position_centered()
-    .opengl()
-    .build()
-    .unwrap();
+    let window = video_subsystem.window("Chip-8 Emulator",
+                                        WINDOW_WIDTH,
+                                        WINDOW_HEIGHT)
+                                .position_centered()
+                                .opengl()
+                                .build()
+                                .unwrap();
     let mut canvas = window.into_canvas().present_vsync().build().unwrap();
     canvas.clear();
     canvas.present();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let mut chip8 = Processor::new();
+
+    let mut rom = File::open(&args[1]).expect("Unable to open file");
+    let mut buffer = Vec::new();
+
+    rom.read_to_end(&mut buffer).unwrap();
+    chip8.load(&buffer);
+
     'gameloop: loop {
         for event in event_pump.poll_iter() {
             match event {
