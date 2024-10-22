fc07d5d677934b42148e01495aff5890cebbbe8b
Jesus Antonio Solis <solisjesus700@gmail.com>
2024-10-21 20:22:45 -0400

    user input, faster tickspeed; added README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Chip-8 Emulator
+
+## Usage
+
+Requires [SDL2 for Rust](https://github.com/Rust-SDL2/rust-sdl2).
+
+In desktop-frontend, do `cargo run path/to/game`.
--- desktop-frontend/src/main.rs
+++ desktop-frontend/src/main.rs
@@ -7,10 +7,12 @@ use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
+use sdl2::keyboard::Keycode;
 
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+const TICKS_PER_FRAME: usize = 10;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -51,11 +53,24 @@ fn main() {
                 Event::Quit{..} => {
                     break 'gameloop;
                 },
+                Event::KeyDown{keycode: Some(key), ..} => {
+                    if let Some(k) = key_to_button(key) {
+                        chip8.keypress(k, true);
+                    }
+                },
+                Event::KeyUp{keycode: Some(key), ..} => {
+                    if let Some(k) = key_to_button(key) {
+                        chip8.keypress(k, false);
+                    }
+                },
                 _ => ()
             }
         }
 
-        chip8.tick();
+        for _ in 0..TICKS_PER_FRAME {
+            chip8.tick();
+        }
+        chip8.tick_timers();
         draw_screen(&chip8, &mut canvas);
     }
 }
@@ -82,4 +97,26 @@ fn draw_screen(processor: &Processor, canvas: &mut Canvas<Window>) {
         }
     }
     canvas.present();
+}
+
+fn key_to_button(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
+    }
 }
